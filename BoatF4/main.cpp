a8b019fba21d697913f595dc0d0c518cb04adc02
GRPlym56 <guy.ringshaw@students.plymouth.ac.uk>
2024-02-14 23:46:24 +0000

    Character transmission

--- BoatF4/main.cpp
+++ BoatF4/main.cpp
@@ -5,11 +5,15 @@ zetaspi Zeta433(f429spi1, PC_7, PA_15);
 
 int main()
 {
+    printf("Starting\n\r");
     Zeta433.altstartup();
-    
+    unsigned char response = Zeta433.readandwrite(0x44); //check CTS byte
+    printf("Zeta response: %x\n\r", response);
     
     while(1)
     {
+        printf("Sending test char X\n\r");
+        Zeta433.sendcharTX('X'); //send test char
         unsigned char response = Zeta433.readandwrite(0x44); //check CTS byte
         printf("Zeta response: %x\n\r", response);
         ThisThread::sleep_for(1000);
